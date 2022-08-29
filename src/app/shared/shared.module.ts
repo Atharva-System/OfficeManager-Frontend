d90d23856759ce96232362ef347c76408a1a9cc8
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 18:40:02 +0530

    added schemas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule } from '@angular/forms';
@@ -11,5 +11,7 @@ import { MenuComponent } from './components/menu/menu.component';
   declarations: [MenuComponent, FilterComponent, FilterPipe, Filter2Component],
   imports: [CommonModule, TranslateModule.forChild(), FormsModule],
   exports: [MenuComponent, FilterComponent, Filter2Component],
+  schemas : [CUSTOM_ELEMENTS_SCHEMA]
+
 })
 export class SharedModule { }
