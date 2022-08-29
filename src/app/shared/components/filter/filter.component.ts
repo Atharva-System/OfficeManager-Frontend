3f90d407dc2531a9eff156232fd4d50c6296e7a1
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 18:29:49 +0530

    added filter with checknox

---
+++ src/app/shared/components/filter/filter.component.ts
@@ -0,0 +1,87 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { createPopper } from '@popperjs/core';
+import { ConstantClass } from 'src/app/shared/constants/constants';
+import { IFilter } from 'src/app/core/shared/interfaces/filter';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss'],
+})
+
+export class FilterComponent implements AfterViewInit {
+  @Input()
+  filterAttributes!: IFilter[];
+  @Input()
+  isSearch!: boolean;
+  @Output() filtredAttributes = new EventEmitter<any>();
+
+  attributes: any[] = [];
+  dropdownPopoverShow = false;
+  searchAttribute!: string;
+
+  @ViewChild('btnDropdownRef', { static: false })
+  btnDropdownRef!: ElementRef;
+  @ViewChild('popoverDropdownRef', { static: false })
+  popoverDropdownRef!: ElementRef;
+
+  constructor(private _eref: ElementRef, public translate : TranslateService) {}
+
+  ngAfterViewInit() {
+    ConstantClass.placement =
+      window.innerHeight < 368 ? 'top-start' : 'bottom-start';
+
+    createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement: ConstantClass.placement,
+      }
+    );
+  }
+
+  //To toggle DropDown
+  toggleDropdown(event: Event) {
+    event.preventDefault();
+    this.dropdownPopoverShow = !this.dropdownPopoverShow;
+  }
+
+  //To hide DropDown on click on outside this component
+  @HostListener(ConstantClass.document.click, ['$event'])
+  public hideDropdown(event: Event) {
+    if (!this._eref.nativeElement.contains(event.target))
+      this.dropdownPopoverShow = false;
+  }
+
+  //To hide DropDown on press on Esc
+  @HostListener(ConstantClass.document.keydown, ['$event']) onKeydownHandler(
+    event: KeyboardEvent
+  ) {
+    if (event.key.toLowerCase() === ConstantClass.escapeKey) {
+      this.dropdownPopoverShow = false;
+    }
+  }
+
+  //On apply Filter
+  onApply() {
+    this.filtredAttributes.emit(this.filterAttributes);
+    this.dropdownPopoverShow = false;
+  }
+
+  //On clear filter
+  onClear() {
+    this.filterAttributes.map((data) => (data.isChecked = false));
+    this.filtredAttributes.emit(this.filterAttributes);
+    this.dropdownPopoverShow = false;
+  }
+}
