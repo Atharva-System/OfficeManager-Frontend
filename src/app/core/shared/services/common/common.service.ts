a4176c4d06fe51a8f22d5aa7130d71b1443f5953
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 14:19:25 +0530

    created common service

---
+++ src/app/core/shared/services/common/common.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { IFilter } from 'src/app/core/shared/interfaces/filter';
+import { IMenuItem } from 'src/app/core/shared/interfaces/menu-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommonService {
+  translation!: string;
+  menuItems: IMenuItem[] = [
+    {
+      name: 'aa',
+      icon: 'assets/svg/heart.svg',
+      isIcon: true,
+    },
+    {
+      name: 'gfg',
+      icon: 'assets/svg/heart.svg',
+      isIcon: true,
+    },
+    {
+      name: 'fffdettttttttttt',
+      icon: 'assets/svg/heart.svg',
+      isIcon: false,
+    },
+    {
+      name: 'bb',
+      icon: '',
+      isIcon: true,
+    },
+  ];
+
+  filterAttributes: IFilter[] = [
+    {
+      isChecked: false,
+      name: 'abc',
+    },
+    {
+      isChecked: false,
+      name: 'fdfdfss',
+    },
+    {
+      isChecked: false,
+      name: 'sfddddddddd',
+    },
+    {
+      isChecked: false,
+      name: 'sfdfs',
+    },
+    {
+      isChecked: false,
+      name: 'dfre',
+    },
+    {
+      isChecked: true,
+      name: 'adfdfbc',
+    },
+    {
+      isChecked: false,
+      name: 'erwre',
+    },
+  ];
+
+  constructor(public translateService: TranslateService) {}
+
+  getTranslateData(text: string) {
+    this.translateService
+      .get(text)
+      .pipe()
+      .subscribe((value) => {
+        this.translation = value;
+      });
+    return this.translation;
+  }
+}
