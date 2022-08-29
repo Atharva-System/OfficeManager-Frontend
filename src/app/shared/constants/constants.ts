edca19407a89ac7c33fa490cf10dfd9f0a357ef1
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 14:22:39 +0530

    created constants file

---
+++ src/app/shared/constants/constants.ts
@@ -0,0 +1,8 @@
+export class ConstantClass {
+  static escapeKey = 'escape';
+  static document = {
+    click: 'document:click',
+    keydown: 'document:keydown',
+  };
+  static placement: any = 'bottom-start';
+}
