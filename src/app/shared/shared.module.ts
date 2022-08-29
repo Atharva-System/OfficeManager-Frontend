f1f9ebc33d486a4c3aedb3fa45c8ef9f7ae1bece
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 14:19:51 +0530

    created shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule } from '@angular/forms';
+import { Filter2Component } from './components/filter2/filter2.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { FilterPipe } from './pipes/filter/filter.pipe';
+import { MenuComponent } from './components/menu/menu.component';
+
+@NgModule({
+  declarations: [MenuComponent, FilterComponent, FilterPipe, Filter2Component],
+  imports: [CommonModule, TranslateModule.forChild(), FormsModule],
+  exports: [MenuComponent, FilterComponent, Filter2Component],
+})
+export class SharedModule { }
