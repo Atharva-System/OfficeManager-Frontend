8daadc77a3aa892381315c833c9322844f4f8b37
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 18:30:08 +0530

    lazy loaded layout module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+  },
+  { path: '**', redirectTo: 'error/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
