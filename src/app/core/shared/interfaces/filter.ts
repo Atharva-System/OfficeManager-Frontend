5e870486de2960564cb5444af01aeab198905964
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-29 14:19:14 +0530

    created model

---
+++ src/app/core/shared/interfaces/filter.ts
@@ -0,0 +1,4 @@
+export interface IFilter {
+    isChecked : boolean,
+    name : string
+}
---
+++ src/app/core/shared/interfaces/menu-item.ts
@@ -0,0 +1,5 @@
+export interface IMenuItem {
+    name : string,
+    icon : string,
+    isIcon : boolean
+}
