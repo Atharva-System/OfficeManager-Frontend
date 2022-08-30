4d2e35394c4729d8065f26f182eddae53ab1fa41
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-30 13:17:19 +0530

    inject commonservice and menu component data get and pass

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/core/layout/services/common/common.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  isOpen = true;
+  menuItems;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public commonService : CommonService) {
+    this.menuItems = this.commonService.menuItems;
   }
 
+  ngOnInit(): void {}
+
+   //To get item on click DropDown MenuItem
+   onItemClick(item: any) {
+    console.log(item);
+  }
 }
