899f4f023034f7d5a107ba22cc1c581cfc722fab
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-30 13:15:50 +0530

    added shared module

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from './header/header.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [ LayoutComponent, HeaderComponent ],
-  imports: [CommonModule, LayoutRoutingModule ],
+  imports: [CommonModule, LayoutRoutingModule, SharedModule],
+  // schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class LayoutModule {}
