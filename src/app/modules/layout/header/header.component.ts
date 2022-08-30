27d531984a72bee7dce0f8db48718a54170668ef
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-30 16:25:49 +0530

    added logic of class add and remove when chnage theme

--- src/app/modules/layout/header/header.component.ts
+++ src/app/modules/layout/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonService } from 'src/app/core/layout/services/common/common.service';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
+import { LayOutCommonService } from 'src/app/core/layout/services/common/common.service';
+import { ConstantClass } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'app-header',
@@ -8,15 +10,45 @@ import { CommonService } from 'src/app/core/layout/services/common/common.servic
 })
 export class HeaderComponent implements OnInit {
   menuItems;
-
-  constructor(public commonService : CommonService) {
-    this.menuItems = this.commonService.menuItems;
+  constructor(
+    public layoutCommonService: LayOutCommonService,
+    private _eref: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.menuItems = this.layoutCommonService.menuItems;
   }
 
   ngOnInit(): void {}
 
-   //To get item on click DropDown MenuItem
-   onItemClick(item: any) {
+  //To get item on click DropDown MenuItem
+  onItemClick(item: any) {
     console.log(item);
+  }
+
+  onThemeChange() {
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        this.document.body.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+        this.layoutCommonService.isDarkTheme = true;
+      } else {
+        this.document.body.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+        this.layoutCommonService.isDarkTheme = false;
+      }
+
+      // if NOT set via local storage previously
+    } else {
+      if (this.document.body.classList.contains('dark')) {
+        this.document.body.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+        this.layoutCommonService.isDarkTheme = false;
+      } else {
+        this.document.body.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+        this.layoutCommonService.isDarkTheme = true;
+      }
+    }
   }
 }
