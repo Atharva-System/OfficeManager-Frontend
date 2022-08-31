ccfd55023253c93225fa97320f9a411e01333ee3
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-31 18:38:01 +0530

    made forgot-password page

--- src/app/modules/auth/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/forgot-password/forgot-password.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ConstantClass } from 'src/app/shared/constants/constants';
+import { Regex } from 'src/app/shared/utils/regex';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
+  public constant;
 
-  constructor() { }
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+      { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
+    ],
+    tnc: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+    ],
+  };
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+    this.initialization();
+    this.constant = ConstantClass;
+  }
+  ngOnInit(): void {}
+
+  initialization() {
+    ConstantClass.forgotPasswordForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.required, Validators.pattern(Regex.emailPattern)],
+      ],
+      tnc: [
+        '',
+        [Validators.required],
+      ],
+    });
   }
 
+  get _forgotPasswordForm() {
+    return ConstantClass.forgotPasswordForm.controls;
+  }
+
+  onSubmit() {
+    this.router.navigate(['auth']);
+    console.log(ConstantClass.forgotPasswordForm.value);
+    this.initialization();
+  }
 }
