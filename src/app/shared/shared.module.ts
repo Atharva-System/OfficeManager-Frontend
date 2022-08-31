0fc3a0cd3d2328107577ee9f576439a176453481
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-31 17:31:54 +0530

    added ReactiveFormsModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Filter2Component } from './components/filter2/filter2.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { FilterPipe } from './pipes/filter/filter.pipe';
@@ -11,8 +11,8 @@ import { InputFieldComponent } from './components/input-field/input-field.compon
 
 @NgModule({
   declarations: [MenuComponent, FilterComponent, FilterPipe, Filter2Component, ButtonComponent, InputFieldComponent],
-  imports: [CommonModule, TranslateModule.forChild(), FormsModule],
-  exports: [MenuComponent, FilterComponent, Filter2Component, ButtonComponent],
+  imports: [CommonModule, TranslateModule.forChild(), FormsModule, ReactiveFormsModule],
+  exports: [MenuComponent, FilterComponent, Filter2Component, ButtonComponent, InputFieldComponent],
   schemas : [CUSTOM_ELEMENTS_SCHEMA]
 
 })
