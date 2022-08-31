e91265fdde8b5b84cb939aaa1f1c3b2434282e07
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-31 17:28:44 +0530

    added reactive form and validation nad submit method

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthCommonService } from 'src/app/core/auth/service/common/auth-common.service';
+import { ConstantClass } from 'src/app/shared/constants/constants';
+import { Regex } from 'src/app/shared/utils/regex';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-  buttonText = 'BUTTON.SIGN_IN';
+  public constant;
+  
+  validationMessages = {
+    email: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+      { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
+    ],
+    password: [
+      { type: 'required', message: 'MESSAGE.REQUIRED' },
+      { type: 'pattern', message: 'MESSAGE.EMAIL_PATTERN' },
+    ],
+  };
 
-  constructor() { }
-
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authCommonService: AuthCommonService,
+  ) {
+    this.initialization();
+    this.constant = ConstantClass;
+  }
   ngOnInit(): void {
+    
+  }
+
+  initialization() {
+    ConstantClass.signinForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.required, Validators.pattern(Regex.emailPattern)],
+      ],
+      password: [
+        '',
+        [Validators.required, Validators.pattern(Regex.passwordPattern)],
+      ],
+    });
+  }
+
+  get _signinForm() {
+    return ConstantClass.signinForm.controls;
+  }
+
+  onSubmit() {
+    
+    localStorage.setItem('loggedIn', btoa(this.constant.signinForm.value.email));
+    this.router.navigate(['dashboard']);
+    this.authCommonService.isLoggedIn = atob(localStorage.getItem('loggedIn') || '');
+    
+    console.log(ConstantClass.signinForm.value);
+    this.initialization();
   }
 }
