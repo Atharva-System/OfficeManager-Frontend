0f2a46429abdd1fd4405bb1dcfffc231962220d4
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-31 17:30:03 +0530

    made custom inputField component and in that used ng-content for get input tag from parent component

--- src/app/shared/components/input-field/input-field.component.ts
+++ src/app/shared/components/input-field/input-field.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.scss']
+  styleUrls: ['./input-field.component.scss'],
 })
 export class InputFieldComponent implements OnInit {
+  @Input() error = false;
+  @Input() icon : string = 'assets/svg/info.svg';
+  
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
