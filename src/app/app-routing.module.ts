f9a376567351e1d3e3ca1aa3479944235583fb1e
Pragati Sanepara <pragati@atharvasystem.com>
2022-08-31 12:42:49 +0530

    added route and lazy-load auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/guard/auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
+    canActivate : [AuthGuard],
     loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule),
   },
   { path: '**', redirectTo: 'error/404' },
 ];
