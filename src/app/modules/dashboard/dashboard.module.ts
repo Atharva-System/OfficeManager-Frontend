cad893304bc55c8af808a3f09ff0f97bfc840fda
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-01 16:05:54 +0530

    Created employee-list component - Initial commit

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
-  declarations: [ DashboardComponent ],
+  declarations: [ DashboardComponent, EmployeeListComponent ],
   imports: [
     CommonModule,
     DashboardRoutingModule
