67efeaa92f63b7fb74c57cd9ba2e882939a4afb7
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-01 12:43:44 +0530

    added comments

--- src/app/shared/pipes/filter/filter.pipe.ts
+++ src/app/shared/pipes/filter/filter.pipe.ts
@@ -5,12 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
   transform(items: any[], args: string, searchField: any[]): any[] | null {
+    // To check if anyone not found from arrguments 
     if (!items || !args || !searchField) {
       return items;
     }
 
     const arg = args.toLowerCase();
 
+    //To return filtered data 
     return items.filter((data) => {
       return searchField.some((key) => {
         return String(data[key]).toLowerCase().includes(arg);
