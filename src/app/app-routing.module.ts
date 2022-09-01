e018f20691af71d833af02501e5d9cd09a2c0bf8
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-01 12:36:11 +0530

    took path from routeConstnat class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/auth/guard/auth.guard';
+import { RouterPathClass } from './shared/constants/route-path';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: RouterPathClass.dashboard,
     canActivate : [AuthGuard],
     loadChildren: () => import('./modules/layout/layout.module').then((m) => m.LayoutModule),
   },
