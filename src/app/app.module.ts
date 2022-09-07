ba396634fabb63ca3bcf4acecdb9acb2680b9271
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-07 15:46:06 +0530

    added GuiGridModule and NgxPaginationModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SharedModule } from './shared/shared.module';
+import { GuiGridModule } from '@generic-ui/ngx-grid';
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
  return new TranslateHttpLoader(http);
 }
@@ -28,6 +29,8 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     }), 
     SharedModule,
     HttpClientModule,
+    GuiGridModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
