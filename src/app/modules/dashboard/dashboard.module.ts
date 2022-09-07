47175eba01264e28b397e9653324677032fd5387
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-07 15:47:16 +0530

    added ReactiveFormsModule, FormsModule, GuiGridModule, NgxPaginationModule, SharedModule

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,15 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { GuiGridModule } from '@generic-ui/ngx-grid';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ DashboardComponent, EmployeeListComponent ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    ReactiveFormsModule,
+    FormsModule,
+    DashboardRoutingModule,
+    GuiGridModule,
+    NgxPaginationModule,
+    SharedModule,
   ],
   schemas : [CUSTOM_ELEMENTS_SCHEMA]
 })
