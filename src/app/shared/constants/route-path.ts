fbf31fa12ae68d602405210cc1802a123d610ab0
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-09 19:28:02 +0530

    added route path

--- src/app/shared/constants/route-path.ts
+++ src/app/shared/constants/route-path.ts
@@ -2,4 +2,6 @@ export class RouterPathClass {
     static auth = 'auth';
     static forgotPassword = 'forgot-password';
     static dashboard = 'dashboard';
+    static employeeList = 'employee-list';
+    static addEmployee = 'add-employee';
 }
