b6a7c14055f219c3f779009d8630ac96ab1e205c
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-09 19:27:46 +0530

    added add-employee route

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouterPathClass } from 'src/app/shared/constants/route-path';
 import { DashboardComponent } from './dashboard.component';
+import { AddEmployeeComponent } from './employee-list/add-employee/add-employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 const routes: Routes = [
@@ -9,8 +11,14 @@ const routes: Routes = [
     component : DashboardComponent,
     children : [
       {
-        path: 'employee-list',
-        component: EmployeeListComponent
+        path: RouterPathClass.employeeList,
+        component: EmployeeListComponent,
+        children : [
+          {
+            path :  RouterPathClass.addEmployee,
+            component : AddEmployeeComponent,
+          }
+        ]
       }
     ]
   }
