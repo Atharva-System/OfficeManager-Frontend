74caf8b48b00b12c594b7ef6d57da70bbb48852b
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-09 19:32:10 +0530

    Created add-employee component

---
+++ src/app/modules/dashboard/employee-list/add-employee/add-employee.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiCallService } from 'src/app/core/dashboard/services/api-call.service';
+import { LayOutCommonService } from 'src/app/core/layout/services/common/common.service';
+import { RouterPathClass } from 'src/app/shared/constants/route-path';
+
+@Component({
+  selector: 'app-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.scss'],
+  // host: {
+  //   class: 'add-employee-sidebar',
+  // },
+})
+export class AddEmployeeComponent implements OnInit {
+  constructor(
+    private router: Router,
+    public layoutCommonService: LayOutCommonService,
+    private activatedRoute: ActivatedRoute,
+    public apiCallService: ApiCallService
+  ) {}
+  ngOnInit(): void {}
+
+  onClose() {
+    // this.apiCallService.isOpenSidebar = false;
+    // setTimeout(() => {
+    this.router.navigate([`../`], {
+      relativeTo: this.activatedRoute,
+    });
+    //   this.apiCallService.pageState = 'left';
+    // }, 1000);
+  }
+}
