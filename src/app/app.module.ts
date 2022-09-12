8fb33a5c1bbfb8dcc39c76449c3de20170f0e34d
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-12 15:41:05 +0530

    added TokenInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { NgxPaginationModule } from 'ngx-pagination';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SharedModule } from './shared/shared.module';
 import { GuiGridModule } from '@generic-ui/ngx-grid';
+import { TokenInterceptor } from './core/auth/interceptor/token.interceptor';
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
- return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http);
 }
 @NgModule({
-  declarations: [
-    AppComponent
-    ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,14 +26,20 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient],
       },
-    }), 
+    }),
     BrowserAnimationsModule,
     SharedModule,
     HttpClientModule,
     GuiGridModule,
-    NgxPaginationModule
+    NgxPaginationModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
