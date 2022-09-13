b00658498707b66afb23863769bd0ab0533beaa1
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-13 14:52:30 +0530

    added svg's path

---
+++ src/app/shared/constants/svgs.ts
@@ -0,0 +1,52 @@
+export class SVGs {
+  //Used in toastr notification
+  static checkBlack = 'assets/svg/check-black.svg';
+  static xCircle = 'assets/svg/x-circle.svg';
+  static alertInfo = 'assets/svg/alert-info.svg';
+  static alertTriangle = 'assets/svg/alert-triangle.svg';
+
+  //Used in menu item
+  static user = 'assets/svg/user.svg';
+  static settings = 'assets/svg/settings.svg';
+  static message = 'assets/svg/message.svg';
+  static signOut = 'assets/svg/sign-out.svg';
+  static info = 'assets/svg/info.svg';
+  static home = 'assets/svg/home.svg';
+  static inbox = 'assets/svg/inbox.svg';
+  static notifications = 'assets/svg/notifications.svg';
+
+  //Used in auth layout
+  static authLayoutImage = 'assets/svg/auth-layout-image.svg';
+  static loginPageLogo = 'assets/svg/login-page-logo.svg';
+  static forgotPassword = 'assets/svg/forgot-password.svg';
+  static email = 'assets/svg/email.svg';
+  static password = 'assets/svg/password.svg';
+
+  //Used in employee list
+  static delete = 'assets/svg/delete.svg';
+
+  //Used in layout
+  static atharvaLogo = 'assets/svg/atharv-icon.png';
+  static atharvaBrandLogoDark = 'assets/svg/atharva-brand-logo-dark.png';
+  static atharvaBrandLogo = 'assets/svg/atharva-brand-logo.png';
+  static sun = 'assets/svg/sun.svg';
+  static moon = 'assets/svg/moon (1).svg';
+  static profile = 'assets/svg/profile.jpg';
+  static hamburgerMenu = 'assets/svg/hamburger-menu.svg';
+
+  //filter component
+  static filter = 'assets/svg/filter.svg';
+  static search = 'assets/svg/search.svg';
+  static close = 'assets/svg/close.svg';
+
+  //input field component
+  static error = 'assets/svg/error.svg';
+
+  //menu component
+  static arrowUp = 'assets/svg/arrow-up.svg';
+  static arrowDown = 'assets/svg/arrow-down.svg';
+
+  //table component
+  static ascending = 'assets/svg/ascending.svg';
+  static descending = 'assets/svg/descending.svg';
+}
