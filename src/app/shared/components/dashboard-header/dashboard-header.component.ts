eaa9229cd5be5e1f103532ad3c05529eaefc3e26
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-13 14:55:40 +0530

    formatted code

--- src/app/shared/components/dashboard-header/dashboard-header.component.ts
+++ src/app/shared/components/dashboard-header/dashboard-header.component.ts
@@ -5,65 +5,72 @@ import { filter, distinctUntilChanged } from 'rxjs';
 @Component({
   selector: 'app-dashboard-header',
   templateUrl: './dashboard-header.component.html',
-  styleUrls: ['./dashboard-header.component.scss']
+  styleUrls: ['./dashboard-header.component.scss'],
 })
 export class DashboardHeaderComponent implements OnInit {
+  public breadcrumbs: any[];
 
-  public breadcrumbs: any[]
- 
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-  ) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
   }
 
   ngOnInit() {
-    this.router.events.pipe(
-      filter((event: any) => event instanceof NavigationEnd),
-      distinctUntilChanged(),
-    ).subscribe(() => {
-      this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
-    })
+    this.router.events
+      .pipe(
+        filter((event: any) => event instanceof NavigationEnd),
+        distinctUntilChanged()
+      )
+      .subscribe(() => {
+        this.breadcrumbs = this.buildBreadCrumb(this.activatedRoute.root);
+      });
   }
 
-  /**
-   * Recursively build breadcrumb according to activated route.
-   * @param route
-   * @param url
-   * @param breadcrumbs
-   */
-  buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: any[] = []): any[] {
+  buildBreadCrumb(
+    route: ActivatedRoute,
+    url: string = '',
+    breadcrumbs: any[] = []
+  ): any[] {
     //If no routeConfig is avalailable we are on the root path
-    let label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data['breadcrumb'] : '';
-    let isClickable = route.routeConfig && route.routeConfig.data && route.routeConfig.data['isClickable'];
-    let path = route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
-  
+    let label =
+      route.routeConfig && route.routeConfig.data
+        ? route.routeConfig.data['breadcrumb']
+        : '';
+    let isClickable =
+      route.routeConfig &&
+      route.routeConfig.data &&
+      route.routeConfig.data['isClickable'];
+    let path =
+      route.routeConfig && route.routeConfig.data ? route.routeConfig.path : '';
+
     // If the route is dynamic route such as ':id', remove it
     const lastRoutePart = path?.split('/').pop();
     const isDynamicRoute = lastRoutePart?.startsWith(':');
-    if(isDynamicRoute && !!route.snapshot) {
+    if (isDynamicRoute && !!route.snapshot) {
       const paramName = lastRoutePart?.split(':')[1];
-      path = path?.replace(lastRoutePart || '', route.snapshot.params['paramName']);
+      path = path?.replace(
+        lastRoutePart || '',
+        route.snapshot.params['paramName']
+      );
       label = route.snapshot.params['paramName'];
     }
-  
+
     //In the routeConfig the complete path is not available,
     //so we rebuild it each time
     const nextUrl = path ? `${url}/${path}` : url;
-  
+
     const breadcrumb: any = {
-        label: label,
-        url: nextUrl,
+      label: label,
+      url: nextUrl,
     };
     // Only adding route with non-empty label
-    const newBreadcrumbs = breadcrumb.label ? [ ...breadcrumbs, breadcrumb ] : [ ...breadcrumbs];
+    const newBreadcrumbs = breadcrumb.label
+      ? [...breadcrumbs, breadcrumb]
+      : [...breadcrumbs];
     if (route.firstChild) {
-        //If we are not on our current path yet,
-        //there will be more children to look after, to build our breadcumb
-        return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+      //If we are not on our current path yet,
+      //there will be more children to look after, to build our breadcumb
+      return this.buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
     }
     return newBreadcrumbs;
   }
-
 }
