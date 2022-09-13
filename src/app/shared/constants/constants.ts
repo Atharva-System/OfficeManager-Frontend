8da4f7a44f91cca75d6e213751b2c90ddfef49b7
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-13 14:52:43 +0530

    added constant

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -1,10 +1,18 @@
-import { FormGroup, Validators } from "@angular/forms";
+import { FormGroup, Validators } from '@angular/forms';
 
 export class ConstantClass {
   static escapeKey = 'escape';
   static darkClass = 'dark';
   static lightClass = 'light';
-  static innerWidth = 800;
+  static innerWidth = {
+    sm: 368,
+    mobile: 500,
+    tablet: 850,
+  };
+  static dropdownPosition = {
+    top: 'top-start',
+    bottom: 'bottom-start',
+  };
   static document = {
     click: 'document:click',
     keydown: 'document:keydown',
@@ -14,12 +22,40 @@ export class ConstantClass {
 
   //validators
   static validators = Validators;
+  static required = 'required';
+  static pattern = 'pattern';
 
   //forms
-  static signinForm : FormGroup;
-  static forgotPasswordForm : FormGroup;
+  static signinForm: FormGroup;
+  static forgotPasswordForm: FormGroup;
 
   //asc-desc
   static asc = 'asc';
   static desc = 'desc';
+
+  //toastr
+  static notificationType = {
+    success: 'success',
+    warning: 'warning',
+    info: 'info',
+    error: 'error',
+  };
+
+  //token on localstorage
+  static token = 'token';
+
+  //Table Related
+  static actions = {
+    edit: 'Edit',
+    delete: 'Delete',
+  };
+  static employeeTable = {
+    isCheckBox: true,
+    totalCount: 0,
+    itemsPerPage: 10,
+    itemsPerPageArr: [10, 20, 25],
+    searchText: 'ta',
+    sorting: '',
+    sortingField: '',
+  };
 }
