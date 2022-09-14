58a6fc989492de5c9747ce408128f41bd3bcda53
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-14 19:30:59 +0530

    added apis

--- src/app/shared/constants/apis.ts
+++ src/app/shared/constants/apis.ts
@@ -1,4 +1,8 @@
 export class APIs {
   static loginApi = 'User/Login';
   static departmentApi = 'Department/All';
+  static employeeApi = 'Employee';
+  static employeeId = 'Employee/2';
+  static employeeEdit = 'Employee/Edit';
+  static employeeAdd = 'Employee/Add';
 }
