ea6ffc42187e6e799b336023c10853fc356df85a
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-14 19:27:59 +0530

    created store for data store

---
+++ src/app/core/shared/models/store.ts
@@ -0,0 +1,22 @@
+import { Observable, BehaviorSubject } from 'rxjs';
+
+export class Store<T> {
+  state$: Observable<T>;
+  private _state$: BehaviorSubject<T>;
+
+  protected constructor(initialState: T) {
+    this._state$ = new BehaviorSubject(initialState);
+    this.state$ = this._state$.asObservable();
+  }
+
+  get state(): T {
+    this.state$.subscribe((data) => {
+    });
+
+    return this._state$.getValue();
+  }
+
+  setState(nextState: T): void {
+    this._state$.next(nextState);
+  }
+}
