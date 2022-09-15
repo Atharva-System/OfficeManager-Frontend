0441949bc6876d17394eb8a80269fffe9f19812e
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-15 15:17:39 +0530

    added refreshToken and logout method

--- src/app/core/auth/service/auth/auth.service.ts
+++ src/app/core/auth/service/auth/auth.service.ts
@@ -1,39 +1,69 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
 import { ApiCallService } from 'src/app/core/dashboard/services/api-call.service';
 import { ApiService } from 'src/app/core/shared/services/api/api.service';
 import { APIs } from 'src/app/shared/constants/apis';
+import { ConstantClass } from 'src/app/shared/constants/constants';
 import { environment } from 'src/environments/environment';
-import { IUser } from '../../models/user';
+import { IUser, LoginResponse } from '../../models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private router: Router, public apiCall: ApiService) { }
+  constructor(public apiCall: ApiService) {}
 
-  login(params:IUser) {
+  login(params: IUser) {
     return this.apiCall
-      .post(
-        APIs.loginApi,
-        params
+      .post(APIs.loginApi, params)
+      .pipe(
+        tap((tokens: LoginResponse) =>
+          this.doLoginUser(params.employeeNo, tokens.data)
+        )
       );
   }
 
-  // logout() {
-  //   this.http
-  //     .post<any>(
-  //       `${environment.apiUrl}/users/revoke-token`,
-  //       {},
-  //       { withCredentials: true }
-  //     )
-  //     .subscribe();
-  //   this.userSubject.next(null);
-  //   //this.router.navigate(['/login']);
-  //   this.router.navigate(['login'], {
-  //     queryParams: { returnUrl: this.router.routerState.snapshot.url },
-  //   });
-  // }
+  logout() {
+    this.doLogoutUser();
+  }
+
+  refreshToken() {
+    return this.apiCall
+      .post(APIs.refreshTokenApi, {
+        refreshToken: this.getRefreshToken(),
+      })
+      .pipe(
+        tap((tokens: LoginResponse) =>
+          this.storeJwtToken(tokens.data.accessToken)
+        )
+      );
+  }
+
+  private doLoginUser(employeeNo: number, tokens: LoginResponse['data']) {
+    this.storeTokens(tokens);
+  }
+
+  private doLogoutUser() {
+    this.removeTokens();
+  }
+
+  private getRefreshToken() {
+    return localStorage.getItem(ConstantClass.refreshToken);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(ConstantClass.token, jwt);
+  }
+
+  private storeTokens(tokens: LoginResponse['data']) {
+    localStorage.setItem(ConstantClass.token, tokens.accessToken);
+    localStorage.setItem(ConstantClass.refreshToken, tokens.refreshToken);
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(ConstantClass.token);
+    localStorage.removeItem(ConstantClass.refreshToken);
+  }
 }
