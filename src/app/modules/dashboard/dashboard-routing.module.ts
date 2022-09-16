5229cdefae1dcdaa2eb895b6919a8d1fa28667a5
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-16 17:24:51 +0530

    added route of department-list and add-department

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RouterPathClass } from 'src/app/shared/constants/route-path';
 import { DashboardComponent } from './dashboard.component';
+import { AddDepartmentComponent } from './department-list/add-department/add-department.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 import { AddEmployeeComponent } from './employee-list/add-employee/add-employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 
@@ -14,14 +16,30 @@ const routes: Routes = [
         path: RouterPathClass.employeeList,
         component: EmployeeListComponent,
         data: {
-          breadcrumb: 'Employee',
+          breadcrumb: RouterPathClass.breadcrumb.employee,
         },
         children: [
           {
             path: RouterPathClass.addEmployee,
             component: AddEmployeeComponent,
             data: {
-              breadcrumb: '',
+              breadcrumb: RouterPathClass.breadcrumb.addEmployee,
+            },
+          },
+        ],
+      },
+      {
+        path: RouterPathClass.departmentList,
+        component: DepartmentListComponent,
+        data: {
+          breadcrumb: RouterPathClass.breadcrumb.department,
+        },
+        children: [
+          {
+            path: RouterPathClass.addDepartment,
+            component: AddDepartmentComponent,
+            data: {
+              breadcrumb: RouterPathClass.breadcrumb.addDepartment,
             },
           },
         ],
