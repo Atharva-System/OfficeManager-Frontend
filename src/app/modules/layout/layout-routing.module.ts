891e477766e9c5b2aec807e464f3a2c8181b5477
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-16 17:26:38 +0530

    added breadcrumb from constant

--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RouterPathClass } from 'src/app/shared/constants/route-path';
 import { LayoutComponent } from './layout.component';
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('../dashboard/dashboard.module').then((m) => m.DashboardModule),
     data: {
-      breadcrumb: 'Dashboard',
+      breadcrumb: RouterPathClass.breadcrumb.dashboard,
     },
   },
   // { path: 'dashboard', redirectTo: 'dashboard', pathMatch: 'full' },
