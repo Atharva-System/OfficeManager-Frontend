4729bdd139d1651e69622315e331169b1b9def04
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-16 17:20:19 +0530

    added DepartmentListComponent, AddDepartmentComponent

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -9,9 +9,11 @@ import { GuiGridModule } from '@generic-ui/ngx-grid';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddEmployeeComponent } from './employee-list/add-employee/add-employee.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { AddDepartmentComponent } from './department-list/add-department/add-department.component';
 
 @NgModule({
-  declarations: [ DashboardComponent, EmployeeListComponent, AddEmployeeComponent ],
+  declarations: [ DashboardComponent, EmployeeListComponent, AddEmployeeComponent, DepartmentListComponent, AddDepartmentComponent ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
