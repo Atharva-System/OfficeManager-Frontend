511496e7a325d5b07c21340ab4fff46da7d6e2df
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-16 17:25:32 +0530

    added department ur

--- src/app/shared/constants/apis.ts
+++ src/app/shared/constants/apis.ts
@@ -1,7 +1,15 @@
 export class APIs {
+  //Auth
   static loginApi = 'User/Login';
   static refreshTokenApi = 'User/RefreshToken';
+
+  //Department
   static departmentApi = 'Department/All';
+  static departmentId = 'Department/2';
+  static departmentEdit = 'Department/Edit';
+  static departmentAdd = 'Department/Add';
+
+  //Employee
   static employeeApi = 'Employee';
   static employeeId = 'Employee/2';
   static employeeEdit = 'Employee/Edit';
