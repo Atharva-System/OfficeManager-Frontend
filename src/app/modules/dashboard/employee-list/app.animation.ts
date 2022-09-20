eb81586db9473fa2f77c4b25a1660eebeb53be27
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-20 18:49:19 +0530

    made delay fast on open component

--- src/app/modules/dashboard/employee-list/app.animation.ts
+++ src/app/modules/dashboard/employee-list/app.animation.ts
@@ -24,7 +24,7 @@ export const slideInAnimation = trigger('slideInAnimation', [
         ':enter',
         [
           style({ transform: 'translateX(100%)' }),
-          animate('1s ease', style({ transform: 'translateX(0%)' })),
+          animate('0.3s ease', style({ transform: 'translateX(0%)' })),
         ],
         { optional: true }
       ),
@@ -32,7 +32,7 @@ export const slideInAnimation = trigger('slideInAnimation', [
         ':leave',
         [
           style({ transform: 'translateX(0%)'}),
-          animate('2s ease-out', style({ transform: 'translateX(100%)' })),
+          animate('0.3s ease-out', style({ transform: 'translateX(100%)' })),
         ],
         { optional: true }
       ),
