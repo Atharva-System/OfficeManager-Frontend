84128f2ab5c829d3cea3166827370e19dd1fe97c
Pragati Sanepara <pragati@atharvasystem.com>
2022-09-22 18:23:00 +0530

    removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { IFilter } from './core/shared/models/filter';
 import { CommonService } from './core/shared/services/common/common.service';
 import { LayOutCommonService } from './core/layout/services/common/common.service';
 import { ConstantClass } from './shared/constants/constants';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -20,20 +17,10 @@ export class AppComponent {
     public commonService: CommonService,
     public layoutCommonService: LayOutCommonService,
     @Inject(DOCUMENT) private document: Document,
-    private spinner: NgxSpinnerService,
-    private router: Router
   ) {
     //Set langulage english
     translate.setDefaultLang('en');
     translate.use('en');
-
-    router.events.subscribe((event) => {
-      if (event instanceof NavigationStart) {
-        this.spinner.show();
-      } else if (event instanceof NavigationEnd) {
-        this.spinner.hide();
-      }
-    });
   }
 
   ngOnInit(): void {
